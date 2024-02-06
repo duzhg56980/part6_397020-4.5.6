3a6f6c90c0637df09daec81ce8f9c68622c3146b
duzhg56980 <guokey1996@gmail.com>
2024-02-06 11:37:59 +0800

    init commit

---
+++ 397020-4.5.6/FunctionUse273.java
@@ -0,0 +1,7 @@
+import cn.hutool.http.Method;
+public class FunctionUse273 {
+public void funcUse() {
+Method method = new Method();
+method.values();
+}
+}
---
+++ 397020-4.5.6/FunctionUse303.java
@@ -0,0 +1,7 @@
+import cn.hutool.http.useragent.Browser;
+public class FunctionUse303 {
+public void funcUse() {
+Browser browser = new Browser();
+browser.isMobile();
+}
+}
